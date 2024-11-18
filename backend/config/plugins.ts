df84680d659d50dce6e41cfbf1b3f5b583c8ef32
martinmanaiBD <martin@borneodigital.co>
2024-11-18 15:49:56 +0800

    (feat)update plugin.ts port smtp

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,11 +1,13 @@
 // config/plugins.ts
+// config/plugins.ts
 export default ({ env }) => ({
   email: {
     config: {
       provider: 'smtp',
       providerOptions: {
         host: env('RESEND_SMTP_HOST', 'smtp.resend.com'),
-        port: env.int('RESEND_SMTP_PORT', 465),
+        port: env.int('RESEND_SMTP_PORT', 465), // Use port 587 for TLS
+        secure: false, // false for port 465 (TLS)
         auth: {
           user: env('RESEND_SMTP_USERNAME'),
           pass: env('RESEND_SMTP_PASSWORD'),
