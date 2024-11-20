241fe2e84701d87bbee39a520603486b949680d3
martinmanaiBD <martin@borneodigital.co>
2024-11-20 11:17:03 +0800

    feat: enhance security and CORS middleware configurations

--- strapi/config/middlewares.ts
+++ strapi/config/middlewares.ts
@@ -1,12 +1,51 @@
-export default [
-  'strapi::logger',
+module.exports = [
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'http:', 'https:', 'https://api.hackwknd.sarawak.digital'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com', 'strapi.io', 'api.hackwknd.sarawak.digital'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'script-src': ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net'],
+          'style-src': ["'self'", "'unsafe-inline'"],
+          'frame-src': ["'self'", 'data:'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://hackwknd.sarawak.digital'], 
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: [
+        'Content-Type',
+        'Authorization',
+        'Origin',
+        'Accept',
+        'Access-Control-Allow-Origin',
+        'Access-Control-Allow-Credentials',
+        'Access-Control-Allow-Headers'
+      ],
+      exposedHeaders: [ // Changed from 'exposed' to 'exposedHeaders'
+        'Content-Range',
+        'X-Total-Count',
+        'Access-Control-Allow-Origin'
+      ],
+      keepHeaderOnError: true,
+      credentials: true,
+      maxAge: 86400 // Optional: Cache preflight response for 1 day
+    },
+  },
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
-  'strapi::public',
-];
+  'strapi::public'
+]
